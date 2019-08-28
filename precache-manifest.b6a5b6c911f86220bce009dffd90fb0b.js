c07267825b425d1be55983a04f79e7f0c4af44ba
Cyber_pineapple <cyber_pineapple@mail.ru>
2019-08-28 14:26:35 +0300

    Updates

---
+++ precache-manifest.b6a5b6c911f86220bce009dffd90fb0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df07614876d00faf47a0d1449e75bed",
+    "url": "/list-module/index.html"
+  },
+  {
+    "revision": "f3ae5c626fe109bb60c8",
+    "url": "/list-module/static/css/main.138948fd.chunk.css"
+  },
+  {
+    "revision": "9527f009edfa4a2a61e7",
+    "url": "/list-module/static/js/2.8804e506.chunk.js"
+  },
+  {
+    "revision": "f3ae5c626fe109bb60c8",
+    "url": "/list-module/static/js/main.831a577d.chunk.js"
+  },
+  {
+    "revision": "a2594989493c40a272c4",
+    "url": "/list-module/static/js/runtime~main.296035f8.js"
+  }
+]);
